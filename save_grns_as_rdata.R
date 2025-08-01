6bb865a3c9296e82cbe9ac834d6adb0cfb22cfe1
karamveervarma37 <karamveervarma37@gmail.com>
2025-08-01 15:09:48 -0400

    Script for Rdata

---
+++ save_grns_as_rdata.R
@@ -0,0 +1,10 @@
+setwd("/gpfs/Home/kmk7420/BEAR-GRN/BEAR-GRN/INFERRED.GRNS")
+#create a empy list to add scRNA and scATAC data
+K562 <- list()
+RNA <- list()
+RNA$CellOracle <- read.csv("K562/CellOracle/K562_human_filtered_out_E7.5_rep1_final_GRN.csv")
+RNA$FigR <- read.csv("K562/FigR/K562_filtered_network.csv")
+
+#add RNA data
+K562$RNA <- RNA
+K562
